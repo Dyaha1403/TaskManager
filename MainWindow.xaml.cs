205a17f9211f1a6c97ba4bb40491b5a4d53c84c3
unknown <Dyaha>
2023-12-24 03:11:14 +0200

    Version 1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CourseProdject.ViewModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CourseProdject
+{
+    public partial class MainWindow : Window
+    {
+        MainViewModel ViewModel;
+        public MainWindow()
+        {
+            //Ініціалізуємо початкові дані
+            ViewModel = new MainViewModel();
+            InitializeComponent();
+            this.DataContext = ViewModel;
+            InitializeAsync();
+        }
+        /// <summary>
+        /// Асинхронний метод ініціалізації вікна.
+        /// </summary>
+        private async void InitializeAsync()
+        {
+            ViewModel.CPUPlot = CPUPlot;
+            ViewModel.MemoryPlot = MemoryPlot;
+
+            CPUPlot.Plot.XLabel("Час");
+            MemoryPlot.Plot.XLabel("Час");
+            CPUPlot.Plot.YLabel("Процесорний час");
+            MemoryPlot.Plot.YLabel("Оперативна пам'ять(MB)");
+            CPUPlot.Plot.Title("Процесор");
+            MemoryPlot.Plot.Title("Пам'ять");
+            //Запускаємо асинхронно дві функції
+            await Task.WhenAll(ViewModel.LoadData(), ViewModel.LoggingData());
+        }
+        /// <summary>
+        /// Обробник події для кнопки завершення процесу.
+        /// </summary>
+        private async void KillProcess_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.TerminateProcess();
+        }
+    }
+}
